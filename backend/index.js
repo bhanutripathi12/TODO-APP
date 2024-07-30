57d07d0963f085898871f256d8a6e549c222c3f7
Asus <bhanu.tripathi12@gmail.com>
2024-07-30 12:39:30 +0530

    first t

--- backend/index.js
+++ backend/index.js
@@ -5,28 +5,34 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
-const userRoutes = require('./routes/user.route');
-const todoRoutes = require('./routes/todo.route');
+const user_routes = require('./routes/user.route'); // Updated path
+const todo_routes = require('./routes/todo.route'); // Updated path
 
 const PORT = process.env.PORT || 8070;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// MongoDB Connection
 const URI = process.env.MONGO_URL;
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB Connection Success!!!'))
-  .catch(err => console.error('MongoDB Connection Error:', err));
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log('MongoDB Connection Success!!!');
+});
 
 // Routes
-app.use('/user', userRoutes);
-app.use('/todo', todoRoutes);
+app.use('/user', user_routes);
+app.use('/todo', todo_routes);
+
+// Handle 404 for unmatched routes
+app.use((req, res, next) => {
+  res.status(404).send('Resource not found');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is up and running at port ${PORT}`);
